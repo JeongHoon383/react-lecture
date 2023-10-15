d82c2fc0d4f4f74af4e611efa7fe26cc550c3bc1
Jeong Hoon <gbg9609@naver.com>
2023-10-15 22:02:28 +0900

    10/15 react-lecture commit

---
+++ first-react/src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import Box from './component/Box';
+
+function App() {
+  return (
+    <div>
+      <div className='box'>
+        Box1
+        <p>리사</p>
+      </div>
+
+      <div className='box'>
+        Box1
+        <p>리사</p>
+      </div>
+    </div>
+
+    
+  )
+}
+
+export default App;
---
+++ first-react/src/component/Box.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+const Box = () => {
+  return (
+      <div className='box'>
+        Box1
+        <p>리사</p>
+      </div>
+    
+  )
+}
+
+export default Box
+
